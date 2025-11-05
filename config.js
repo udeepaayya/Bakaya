584f4d6bef9d0780b684c9c8c00da0d4eeccfcfd
udeepaayya <udeepaayyaofc@gmail.com>
2025-11-05 20:25:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "M3FGVJBJ#vQOoJTGPRMRxbz5JMdiDKqcD9rAuO_h1SAhmXiWuHFE",
-  MONGODB: process.env.MONGODB || "mongodb://mongo:wOWITGyKuGkFUYAVcdpUmJGtbzewRZpZ@yamabiko.proxy.rlwy.net:36560",
+  SESSION_ID: process.env.SESSION_ID || "rY9kHQRS#f3UuXPfDWVP7EIgFcY6zC-mbme_vITh1_AHSkUkue3E",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:ypcMCLLXuIWINczNWvkwtdvjoEhVtPEf@trolley.proxy.rlwy.net:36396",
   OWNER_NUM: process.env.OWNER_NUM || "94762095304",
 };
